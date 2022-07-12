53aadd30a759b958ab38d83d6bf4666e9aaff44d
unknown <yejz0715@gmail.com>
2022-07-12 14:54:20 +0900

    UPDATE :image 파일경로 변경

--- src/Pages/IntroPage/IntroPresenter.jsx
+++ src/Pages/IntroPage/IntroPresenter.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import * as S from "./style.js";
-import UserImg from "../../assets/intro_img.png";
-import Logo from "../../assets/intro_logo.png";
+//import UserImg from "../../assets/intro_img.png";
+//import Logo from "../../assets/intro_logo.png";
 const IntroPresenter = ({ handleOnClickPage, isClicked }) => (
   <S.Container onClick={handleOnClickPage} isClicked={isClicked}>
     <S.ImgWrapper>
-      <img src={UserImg} alt="userImage" width="230px" />
+      <img src={"/assets/intro_img.png"} alt="userImage" width="230px" />
     </S.ImgWrapper>
-    <img src={Logo} alt="logo" width="352px" />
+    <img src={"/assets/intro_logo.png"} alt="logo" width="352px" />
   </S.Container>
 );
 
