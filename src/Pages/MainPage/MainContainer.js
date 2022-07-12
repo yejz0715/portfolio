181446c3e7011db29785513a72978f71975d795d
unknown <yejz0715@gmail.com>
2022-07-12 14:55:08 +0900

    CREATE:mainPage 생성

--- src/Pages/MainPage/MainContainer.js
+++ src/Pages/MainPage/MainContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MainPresenter from "./MainPresenter";
+import { projectList } from "../../data/list/dummy";
 const MainContainer = () => {
-  return <MainPresenter />;
+  return <MainPresenter projectList={projectList} />;
 };
 export default MainContainer;
--- src/Pages/MainPage/MainPresenter.jsx
+++ src/Pages/MainPage/MainPresenter.jsx
@@ -1,4 +1,16 @@
 import React from "react";
-import "./style.css";
-const MainPresenter = () => <div></div>;
+import Header from "../../Component/Header";
+import Profile from "../../Component/Profile";
+import Project from "../../Component/Project";
+import Story from "../../Component/Story";
+import * as S from "./style.js";
+const MainPresenter = ({ projectList }) => (
+  <S.Container>
+    {console.log(projectList)}
+    <Header />
+    <Profile />
+    <Story />
+    <Project />
+  </S.Container>
+);
 export default MainPresenter;
