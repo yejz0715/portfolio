a3b0603ee99bd5d2f8e905d2dc47d410601c4b7e
unknown <yejz0715@gmail.com>
2022-07-14 15:24:41 +0900

    FIX:project-component 수정

--- src/Component/Project/index.js
+++ src/Component/Project/index.js
@@ -1,23 +1,21 @@
 import React from "react";
 import * as S from "./style.js";
 const Project = ({
-  id,
-  name,
-  period,
-  intro,
-  skill,
-  gitUrl,
-  demoUrl,
+  // id,
+  // name,
+  // period,
+  // intro,
+  // skill,
+  // gitUrl,
+  // demoUrl,
+  // tag,
   images,
-  tag,
   handleOnClick,
-  isShow,
 }) => {
   return (
     <S.Container
       style={{ backgroundImage: `url(${images[0]})` }}
       onClick={handleOnClick}
-      isShow={isShow}
     ></S.Container>
   );
 };
