f5a8ef7fec8b5121e2604feddc7dc442c5b7130d
unknown <yejz0715@gmail.com>
2022-07-14 15:22:17 +0900

    STYLE:팝업 스타일 수정

--- src/Component/PopUp/style.js
+++ src/Component/PopUp/style.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 export const container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 1304px;
   height: 892px;
-  background-color: #f5f5f5;
+  background-color: #000000;
+  margin: 60px 110px;
 `;
