9d2b5ff8383ba66b629485f16bcc0c51316c780f
unknown <yejz0715@gmail.com>
2022-07-17 03:31:14 +0900

    UPDATE:popContents 컴포넌트 ui 및 스타일 수정

--- src/Component/PopImgSlider/style.js
+++ src/Component/PopImgSlider/style.js
@@ -1,6 +1,34 @@
 import styled from "styled-components";
+import Slider from "react-slick";
+
 export const Container = styled.div`
-  width: 860px;
-  height: 860px;
-  background-color: aqua;
+  width: 800px;
+  height: 800px;
+  background-color: transparent;
+`;
+export const customSlider = styled(Slider)`
+  .slick-prev:before {
+    font-size: 30px !important;
+  }
+  .slick-next:before {
+    font-size: 30px !important;
+  }
+  .slick-prev {
+    left: -50px;
+  }
+
+  .slick-prev.slick-disabled:before {
+    opacity: 0.6;
+  }
+`;
+
+export const img = styled.div`
+  width: 100%;
+  height: 800px;
+  background-image: ${(props) =>
+    props.imgPath ? `url(${props.imgPath})` : "none"};
+
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
 `;
