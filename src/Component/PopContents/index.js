b00439875c25cacac7e7459e9399dd67c0197dac
unknown <yejz0715@gmail.com>
2022-07-17 03:30:02 +0900

    UPDATE:popContents 컴포넌트 ui 및 스타일 수정

--- src/Component/PopContents/index.js
+++ src/Component/PopContents/index.js
@@ -1,6 +1,33 @@
 import React from "react";
 import * as S from "./style.js";
-const PopContents = () => {
-  return <S.container />;
+const PopContents = ({
+  id,
+  name,
+  period,
+  intro,
+  skill,
+  responsibilities,
+  gitUrl,
+  demoUrl,
+  tag,
+}) => {
+  return (
+    <S.container>
+      <S.box>yejx0715</S.box>
+      <S.wrapper>
+        <S.text>{id} 번</S.text>
+        <S.text>포트폴리오 이름: {name}</S.text>
+        <S.text>기간: {period}</S.text>
+        <S.text>한줄 소개: {intro}</S.text>
+        <S.text>skill: {skill}</S.text>
+        <S.text>담당 업무: {responsibilities}</S.text>
+        <S.text>git: {gitUrl}</S.text>
+        <S.text>{demoUrl}</S.text>
+        {tag.map((item) => (
+          <S.tag># {item}</S.tag>
+        ))}
+      </S.wrapper>
+    </S.container>
+  );
 };
 export default PopContents;
--- src/Component/PopContents/style.js
+++ src/Component/PopContents/style.js
@@ -4,5 +4,29 @@ export const container = styled.div`
   width: 444px;
   height: 800px;
 `;
-
-export const wrapper = styled.div``;
+export const box = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 64px;
+  border-bottom: 1px solid #d9d9d9;
+  margin-bottom: 30px;
+  font-size: 25px;
+`;
+export const wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const text = styled.p`
+  padding: 5px 20px;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 23px;
+`;
+export const tag = styled.label`
+  padding: 5px 20px;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 23px;
+  color: rgba(5, 59, 110, 1);
+`;
