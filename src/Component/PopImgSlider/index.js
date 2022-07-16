8f1876d7f2227aa57c43377ac66495563fb75f1d
unknown <yejz0715@gmail.com>
2022-07-17 03:30:54 +0900

    UPDATE:popContents 컴포넌트 ui 및 스타일 수정

--- src/Component/PopImgSlider/index.js
+++ src/Component/PopImgSlider/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 import * as S from "./style.js";
-const PopImgSlider = () => {
-  return <S.Container />;
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+
+const PopImgSlider = ({ images, id }) => {
+  const settings = {
+    dots: true,
+    fade: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <S.Container>
+      <S.customSlider {...settings}>
+        {images.map((item) => (
+          <S.img key={id} imgPath={item} />
+        ))}
+      </S.customSlider>
+    </S.Container>
+  );
 };
+
 export default PopImgSlider;
