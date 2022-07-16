3437b1da29249b62f36e90104fa6c9f1b66692f0
unknown <yejz0715@gmail.com>
2022-07-17 03:29:09 +0900

    UPDATE:popUp 컴포넌트 ui 및 스타일 수정

--- src/Component/PopUp/index.js
+++ src/Component/PopUp/index.js
@@ -2,11 +2,39 @@ import React from "react";
 import PopContents from "../PopContents/index.js";
 import PopImgSlider from "../PopImgSlider/index.js";
 import * as S from "./style.js";
-const PopUp = () => {
+const PopUp = ({
+  id,
+  name,
+  period,
+  intro,
+  skill,
+  responsibilities,
+  gitUrl,
+  demoUrl,
+  images,
+  tag,
+  setShowProject,
+}) => {
   return (
     <S.container>
-      <PopImgSlider />
-      <PopContents />
+      <PopImgSlider id={id} images={images} />
+
+      <PopContents
+        id={id}
+        name={name}
+        period={period}
+        intro={intro}
+        skill={skill}
+        responsibilities={responsibilities}
+        gitUrl={gitUrl}
+        demoUrl={demoUrl}
+        tag={tag}
+      />
+      <S.deleteButton
+        onClick={() => {
+          setShowProject(false);
+        }}
+      />
     </S.container>
   );
 };
--- src/Component/PopUp/style.js
+++ src/Component/PopUp/style.js
@@ -1,10 +1,20 @@
 import styled from "styled-components";
+import { FiXSquare } from "react-icons/fi";
 export const container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 1304px;
-  height: 800px;
-  background-color: #d4d4d4;
-  margin: 60px 110px;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+export const deleteButton = styled(FiXSquare)`
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  width: 36px;
+  height: 36px;
+  padding: 10px;
+  color: #d5d5d5;
 `;
