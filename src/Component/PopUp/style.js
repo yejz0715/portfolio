de0d3fa183f57f4a77118c137eb47efe1600c509
unknown <yejz0715@gmail.com>
2022-07-17 17:24:26 +0900

    STYLE:popup-컴포넌트 스타일 수정

--- src/Component/PopUp/style.js
+++ src/Component/PopUp/style.js
@@ -1,20 +1,23 @@
 import styled from "styled-components";
-import { FiXSquare } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
 export const container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
-  height: 100vh;
   overflow: hidden;
+  width: 100%;
+  height: 100%;
+  min-height: 720px;
+  max-height: 891px;
 `;
 
-export const deleteButton = styled(FiXSquare)`
+export const deleteButton = styled(FiX)`
   position: absolute;
-  top: 30px;
-  right: 30px;
+  top: 3px;
+  right: 3px;
   width: 36px;
   height: 36px;
   padding: 10px;
   color: #d5d5d5;
+  cursor: pointer;
 `;
