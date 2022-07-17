0abefe3662cb62f7f2ad7fccb4bdf761cb726c98
unknown <yejz0715@gmail.com>
2022-07-17 17:26:39 +0900

    STYLE:project-컴포넌트 스타일 수정

--- src/Component/Project/style.js
+++ src/Component/Project/style.js
@@ -3,4 +3,7 @@ export const Container = styled.img`
   width: 290px;
   height: 290px;
   background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  cursor: pointer;
 `;
