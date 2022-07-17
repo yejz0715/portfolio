d07741c0a05d1ee9322b47686b624dac678dc0a5
unknown <yejz0715@gmail.com>
2022-07-18 01:01:00 +0900

    UPDATE:header-컴포넌트 수정, rename

--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -1,32 +1,39 @@
 import React from "react";
 import * as S from "./style.js";
 //import headerLogo from "../../assets/header_logo.png";
-import { FiHome, FiGithub, FiSearch } from "react-icons/fi";
+import { FiHome, FiGithub } from "react-icons/fi";
 import { BiPencil } from "react-icons/bi";
 const Header = ({ handleOnChange, handleOnKeyEnter }) => {
   return (
-    <S.headerContainer>
-      <S.headerLogo
+    <S.HeaderContainer>
+      <S.HeaderLogo
         src={process.env.PUBLIC_URL + "/images/header_logo.png"}
         alt="logo"
       />
-      <S.searchInput
+      <S.SearchInput
         type="text"
         onChange={handleOnChange}
-        placeholder="ex) javascript"
+        placeholder="개발 스택을 입력하고 enter !"
         onKeyPress={handleOnKeyEnter}
       />
-      <S.headerIcon>
+      <S.HeaderIconWrapper>
         <FiHome size="24px" cursor="pointer" />
-        <a href="https://github.com/yejz0715" target="_blank" rel="noreferrer">
-          <FiGithub size="24px" cursor="pointer" />
-        </a>
+        <S.HeaderIcon>
+          <a
+            href="https://github.com/yejz0715"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiGithub size="24px" cursor="pointer" />
+          </a>
 
+          <S.HeaderIconLabel>github</S.HeaderIconLabel>
+        </S.HeaderIcon>
         <a href="https://velog.io/@yejz0715" target="_blank" rel="noreferrer">
           <BiPencil size="24px" cursor="pointer" />
         </a>
-      </S.headerIcon>
-    </S.headerContainer>
+      </S.HeaderIconWrapper>
+    </S.HeaderContainer>
   );
 };
 export default Header;
--- src/Component/Header/style.js
+++ src/Component/Header/style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-export const headerContainer = styled.div`
+export const HeaderContainer = styled.div`
   height: 60px;
   display: flex;
   border: 1px solid #efefef;
@@ -8,12 +8,12 @@ export const headerContainer = styled.div`
   align-items: center;
   justify-content: center;
 `;
-export const headerLogo = styled.img`
+export const HeaderLogo = styled.img`
   width: 115px;
   cursor: pointer;
 `;
 
-export const searchInput = styled.input`
+export const SearchInput = styled.input`
   width: 252px;
   height: 36px;
   background-color: #efefef;
@@ -21,16 +21,35 @@ export const searchInput = styled.input`
   border: none;
   padding-left: 20px;
   margin: 10px 150px;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
-// searchInput.focus
-//   border: none;
-//   outline: none;
-// ;
-
-export const headerIcon = styled.ul`
+export const HeaderIconWrapper = styled.ul`
+  position: relative;
   display: flex;
   list-style: none;
   width: 120px;
   justify-content: space-between;
+`;
+
+export const HeaderIconLabel = styled.label`
+  position: absolute;
+  top: 50px;
+  display: none;
+  background-color: black;
+  color: #fff;
+`;
+
+export const HeaderIcon = styled.li`
+  display: flex;
+  flex-direction: column;
+
+  &:hover {
+    ${HeaderIconLabel} {
+      display: inline;
+    }
+  }
 `;
