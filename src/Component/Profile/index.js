40bc1b47a7b9153ba6b85ff015cbea7abd13ffad
unknown <yejz0715@gmail.com>
2022-07-17 17:25:45 +0900

    UPDATE:profile-컴포넌트 ui 및 스타일 수정

--- src/Component/Profile/index.js
+++ src/Component/Profile/index.js
@@ -2,22 +2,30 @@ import React from "react";
 import * as S from "./style.js";
 const Profile = ({ handleShowStory }) => {
   return (
-    <S.Container onClick={handleShowStory}>
-      <S.ProfileImg src={"/assets/intro_img.png"} alt="image" />
+    <S.Container>
+      <S.ProfileImg
+        src={"/assets/user_yeji.jpg"}
+        alt="image"
+        onClick={handleShowStory}
+      />
       <S.ProfileText>
         <S.Wrapper>
           <S.Id>yejz0715</S.Id>
-          <S.MailButton>
+          <S.MailButton href="mailto:yejz0715@gmail.com">
             메일 보내기
             <S.sendIcon />
           </S.MailButton>
         </S.Wrapper>
         <S.Mail>
-          <S.mailIcon />
+          <img
+            src="assets/gmail_icon.svg"
+            alt="gmail"
+            width="22px"
+            style={{ marginRight: "4px" }}
+          />
           yejz0715@gmail.com
         </S.Mail>
-        <S.Name>이예지</S.Name>
-        <S.Contents>안녕안녕안녕</S.Contents>
+        <S.Contents>안녕하세요! 프론트엔드 개발자 이예지입니다!</S.Contents>
       </S.ProfileText>
     </S.Container>
   );
--- src/Component/Profile/style.js
+++ src/Component/Profile/style.js
@@ -2,28 +2,32 @@ import styled from "styled-components";
 import { FiMail, FiSend } from "react-icons/fi";
 
 export const Container = styled.div`
-  width: 623px;
   height: 176px;
   display: flex;
   margin: 60px 0;
   padding-right: 130px;
   background-color: transparent;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const ProfileImg = styled.img`
   width: 180px;
   height: 180px;
-  background-color: #ffffff;
+  background-color: transparent;
   border-radius: 50%;
+  cursor: pointer;
+  border: 2px solid #d9d9d9;
+  padding: 6px;
 `;
 
 export const ProfileText = styled.div`
   display: flex;
   flex-direction: column;
   width: 380px;
-  height: 200px;
   margin-left: 60px;
   padding-top: 10px;
+  cursor: default;
 `;
 export const Wrapper = styled.div`
   display: flex;
@@ -31,34 +35,31 @@ export const Wrapper = styled.div`
 
 export const Id = styled.span`
   font-size: 28px;
-  font-weight: 600;
   line-height: 33px;
-  margin-bottom: 12px;
+  margin-bottom: 24px;
 `;
-export const MailButton = styled.button`
+export const MailButton = styled.a`
   width: 120px;
   height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: #f9f9f9;
   border: 1px solid #efefef;
   border-radius: 3px;
   margin-left: 32px;
-  line-height: 16px;
   font-size: 14px;
-  text-align: center;
+  cursor: pointer;
 `;
 export const sendIcon = styled(FiSend)`
   size: 20px;
-  padding: 5px 0 0 6px;
 `;
 
-export const mailIcon = styled(FiMail)`
-  size: 22px;
-  margin-right: 6px;
-  color: #720000;
-`;
 export const Mail = styled.div`
   display: flex;
+  align-items: center;
   font-size: 20px;
+  line-height: 20px;
   margin-bottom: 19px;
 `;
 export const Name = styled.label`
@@ -68,6 +69,7 @@ export const Name = styled.label`
   margin-bottom: 4px;
 `;
 export const Contents = styled.label`
+  width: 100%;
   color: #646464;
   font-size: 20px;
   font-weight: 400;
