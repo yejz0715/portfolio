223aed178c1bdbfa90c0eec2a6a5703480a9f3ab
unknown <yejz0715@gmail.com>
2022-07-18 12:53:29 +0900

    UPDATE: popImgSlider-컴포넌트 스타일 변경

--- src/Component/PopImgSlider/style.js
+++ src/Component/PopImgSlider/style.js
@@ -2,14 +2,17 @@ import styled from "styled-components";
 import Slider from "react-slick";
 
 export const Container = styled.div`
+  position: relative;
   width: 800px;
   height: 90%;
   min-height: 720px;
   background-color: black;
   display: flex;
-  align-items: center;
+  align-items: flex-start;
 `;
 export const CustomSlider = styled(Slider)`
+  width: 100%;
+  height: 100%;
   .slick-prev:before {
     font-size: 30px !important;
   }
@@ -31,6 +34,7 @@ export const CustomSlider = styled(Slider)`
 export const Img = styled.div`
   width: 100%;
   height: 720px;
+  margin-top: 40px;
   background-image: ${(props) =>
     props.imgPath ? `url(${props.imgPath})` : "none"};
 
