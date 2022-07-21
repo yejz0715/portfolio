d3501f2c4e5f43b66610c29423ba5d9303b78dbc
unknown <yejz0715@gmail.com>
2022-07-22 03:18:32 +0900

    UPDATE:에러페이지 생성

---
+++ src/Pages/ErrorPage/NotFoundContainer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import NotFoundPresenter from "./NotFoundPresenter";
+const NotFoundContainer = () => {
+  return <NotFoundPresenter />;
+};
+export default NotFoundContainer;
---
+++ src/Pages/ErrorPage/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const Container = styled.div`
+  display: flex;
+  background-color: transparent;
+  font-size: 100px;
+  font-weight: bold;
+`;
