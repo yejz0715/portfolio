31295840f6a39314ccfc25e17113f062b6752d02
unknown <yejz0715@gmail.com>
2022-07-23 21:03:13 +0900

    ADD:introPage-링크추가

--- src/Pages/IntroPage/IntroPresenter.jsx
+++ src/Pages/IntroPage/IntroPresenter.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import * as S from "./style.js";
 const IntroPresenter = ({ handleOnClickPage, isClicked }) => (
   <S.Container onClick={handleOnClickPage} isClicked={isClicked}>
-    <S.ImgWrapper>
+    <a
+      href="https://yejz0715.github.io/portfolio/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <S.ImgWrapper>
+        <img
+          src={process.env.PUBLIC_URL + "/images/intro_img.png"}
+          alt="userImage"
+          width="230px"
+        />
+      </S.ImgWrapper>
       <img
-        src={process.env.PUBLIC_URL + "/images/intro_img.png"}
-        alt="userImage"
-        width="230px"
+        src={process.env.PUBLIC_URL + "/images/intro_logo.png"}
+        alt="logo"
+        width="352px"
       />
-    </S.ImgWrapper>
-    <img
-      src={process.env.PUBLIC_URL + "/images/intro_logo.png"}
-      alt="logo"
-      width="352px"
-    />
+      <S.Message>포트폴리오 사이트로 이동하려면 클릭해주세요!</S.Message>
+    </a>
   </S.Container>
 );
 
