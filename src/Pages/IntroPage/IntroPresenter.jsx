5859c7933e5e72e73d8d3d220160016716e5d966
unknown <yejz0715@gmail.com>
2022-07-27 18:51:05 +0900

    UPDATE:introPage 수정

--- src/Pages/IntroPage/IntroPresenter.jsx
+++ src/Pages/IntroPage/IntroPresenter.jsx
@@ -2,25 +2,19 @@ import React from "react";
 import * as S from "./style.js";
 const IntroPresenter = ({ handleOnClickPage, isClicked }) => (
   <S.Container onClick={handleOnClickPage} isClicked={isClicked}>
-    <a
-      href="https://yejz0715.github.io/portfolio/"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <S.ImgWrapper>
-        <img
-          src={process.env.PUBLIC_URL + "/images/intro_img.png"}
-          alt="userImage"
-          width="230px"
-        />
-      </S.ImgWrapper>
+    <S.ImgWrapper>
       <img
-        src={process.env.PUBLIC_URL + "/images/intro_logo.png"}
-        alt="logo"
-        width="352px"
+        src={process.env.PUBLIC_URL + "/images/intro_img.png"}
+        alt="userImage"
+        width="230px"
       />
-      <S.Message>포트폴리오 사이트로 이동하려면 클릭해주세요!</S.Message>
-    </a>
+    </S.ImgWrapper>
+    <img
+      src={process.env.PUBLIC_URL + "/images/intro_logo.png"}
+      alt="logo"
+      width="352px"
+    />
+    <S.Message>포트폴리오 사이트로 이동하려면 클릭해주세요!</S.Message>
   </S.Container>
 );
 
