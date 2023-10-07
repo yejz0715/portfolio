a697fb40df56c4cb398cc58d04a93557b4488cc6
yejz0715 <yejz0715@gmail.com>
2023-10-07 20:14:53 +0900

    style 수정

--- src/Component/PopContents/style.js
+++ src/Component/PopContents/style.js
@@ -1,69 +1,69 @@
 import styled from "styled-components";
 export const Container = styled.div`
-  position: relative;
-  background-color: #ffffff;
-  width: 421px;
-  box-sizing: border-box;
-  height: 100%;
-  min-height: 720px;
+    position: relative;
+    background-color: #ffffff;
+    width: 421px;
+    box-sizing: border-box;
+    height: 100%;
+    min-height: 720px;
 `;
 
 export const UserImg = styled.div`
-  border-radius: 50%;
-  width: 36px;
-  height: 36px;
-  background-repeat: no-repeat;
-  background-size: cover;
-  border: 2px solid #d9d9d9;
-  margin-right: 8px;
-  background-image: ${(props) => (props.path ? `url(${props.path})` : "none")};
+    border-radius: 50%;
+    width: 36px;
+    height: 36px;
+    background-repeat: no-repeat;
+    background-size: cover;
+    margin-right: 8px;
+    background-image: ${(props) =>
+        props.path ? `url(${props.path})` : "none"};
 `;
 export const UserBox = styled.div`
-  display: flex;
-  align-items: center;
-  height: 54px;
-  border-bottom: 1px solid #d9d9d9;
-  font-size: 23px;
-  font-family: "GangwonEdu_OTFBoldA";
-  line-height: 54px;
-  padding-left: 20px;
-  cursor: default;
+    display: flex;
+    align-items: center;
+    height: 54px;
+    border-bottom: 1px solid #d9d9d9;
+    font-size: 23px;
+    font-family: "GangwonEdu_OTFBoldA";
+    line-height: 54px;
+    padding-left: 20px;
+    cursor: default;
 `;
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0px 20px;
+    display: flex;
+    flex-direction: column;
+    margin: 0px 20px;
 `;
 
 export const Tag = styled.p`
-  font-size: 18px;
-  font-weight: bold;
-  line-height: 23px;
-  color: rgba(5, 59, 110, 1);
+    font-size: 18px;
+    font-weight: bold;
+    line-height: 23px;
+    color: rgba(5, 59, 110, 1);
 `;
 
 export const SubTitle = styled.label`
-  font-size: 19px;
-  margin-top: 16px;
-  margin-bottom: 8px;
+    font-size: 19px;
+    margin-top: 16px;
+    margin-bottom: 8px;
 `;
 export const Description = styled.label`
-  color: ${(props) =>
-    props.type === "link" ? "rgba(5, 59, 110, 1);" : "#646464"};
-  cursor: ${(props) => (props.type === "link" ? "pointer" : "default")};
-  font-size: 18px;
-  line-height: 21px;
-  margin-bottom: 5px;
-  display: block;
+    color: ${(props) =>
+        props.type === "link" ? "rgba(5, 59, 110, 1);" : "#646464"};
+    cursor: ${(props) => (props.type === "link" ? "pointer" : "default")};
+    font-size: 18px;
+    line-height: 21px;
+    margin-bottom: 5px;
+    display: block;
 `;
 
 export const DevTime = styled.label`
-  position: absolute;
-  bottom: 13px;
-  left: 20px;
-  font-size: 16px;
-  color: gray;
+    position: absolute;
+    bottom: 13px;
+    left: 20px;
+    font-size: 16px;
+    color: gray;
 `;
 export const Link = styled.div`
-  margin: 10px 0;
+    margin: 10px 0;
 `;
--- src/Component/Profile/style.js
+++ src/Component/Profile/style.js
@@ -2,110 +2,107 @@ import styled from "styled-components";
 import { FiSend } from "react-icons/fi";
 
 export const Container = styled.div`
-  height: 176px;
-  display: flex;
-  margin: 60px 0;
-  padding-right: 130px;
-  background-color: transparent;
-  align-items: center;
-  justify-content: center;
+    height: 176px;
+    display: flex;
+    margin: 60px 0;
+    padding-right: 130px;
+    background-color: transparent;
+    align-items: center;
+    justify-content: center;
 `;
 export const ProfileImgLabel = styled.label`
-  position: absolute;
-  top: -25px;
-  display: none;
-  height: 20px;
-  line-height: 24px;
-  background-color: #f5c125;
-  border-radius: 2px;
-  font-weight: bold;
-  color: #fff;
-  padding: 3px 3px 2px 3px;
+    position: absolute;
+    top: -25px;
+    display: none;
+    height: 20px;
+    line-height: 24px;
+    background-color: #f5c125;
+    border-radius: 2px;
+    font-weight: bold;
+    color: #fff;
+    padding: 3px 3px 2px 3px;
 `;
 export const ProfileImgWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  cursor: pointer;
-  &:hover {
-    ${ProfileImgLabel} {
-      display: inline;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    cursor: pointer;
+    &:hover {
+        ${ProfileImgLabel} {
+            display: inline;
+        }
     }
-  }
 `;
 
 export const ProfileImg = styled.img`
-  width: 160px;
-  height: 160px;
-  background-color: transparent;
-  border-radius: 50%;
-
-  border: 2px solid #d9d9d9;
-  padding: 8px 3px 8px 3px;
+    width: 167px;
+    height: 167px;
+    background-color: transparent;
+    border-radius: 50%;
 `;
 
 export const ProfileText = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 397px;
-  margin-left: 60px;
-  padding-top: 10px;
-  cursor: default;
+    display: flex;
+    flex-direction: column;
+    width: 397px;
+    margin-left: 70px;
+    padding-top: 10px;
+    cursor: default;
 `;
 export const Wrapper = styled.div`
-  display: flex;
+    display: flex;
 `;
 
 export const Id = styled.span`
-  font-family: "GangwonEdu_OTFBoldA";
-  font-size: 30px;
-  line-height: 33px;
-  margin-bottom: 24px;
+    font-family: "GangwonEdu_OTFBoldA";
+    font-size: 30px;
+    line-height: 33px;
+    margin-bottom: 24px;
 `;
 export const MailButton = styled.a`
-  width: 120px;
-  height: 27px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #fafafa;
-  border: 1px solid #d3d3d3;
-  box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.1);
-  border-radius: 3px;
-  margin-left: 32px;
-  font-size: 14px;
-  padding-top: 5px;
-  cursor: pointer;
+    width: 120px;
+    height: 27px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fafafa;
+    border: 1px solid #d3d3d3;
+    box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+    margin-left: 32px;
+    font-size: 14px;
+    padding-top: 5px;
+    cursor: pointer;
 `;
 export const SendIcon = styled(FiSend)`
-  size: 20px;
+    size: 20px;
 `;
 
 export const Mail = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 21px;
-  line-height: 20px;
-  margin-bottom: 19px;
+    display: flex;
+    align-items: center;
+    font-size: 21px;
+    line-height: 20px;
+    margin-bottom: 19px;
 `;
 
 export const MailImg = styled.img`
-  width: 22px;
-  margin-right: 5px;
+    width: 22px;
+    margin-right: 5px;
 `;
 export const Name = styled.label`
-  font-size: 21px;
-  line-height: 23.44px;
-  margin-bottom: 4px;
+    font-size: 21px;
+    line-height: 23.44px;
+    margin-bottom: 4px;
 `;
 export const Contents = styled.label`
-  width: 100%;
-  color: #646464;
-  font-size: 18px;
-  line-height: 22px;
-  margin-bottom: 8px;
+    width: 100%;
+    color: #646464;
+    font-size: 18px;
+    line-height: 22px;
+    margin-bottom: 8px;
 `;
 export const Tag = styled.label`
-  color: rgba(5, 59, 110, 1);
-  font-size: 15px;
+    color: rgba(5, 59, 110, 1);
+    font-size: 15px;
 `;
